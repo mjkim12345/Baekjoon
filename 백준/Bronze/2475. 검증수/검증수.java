f72af9f5d56ab14ae150df7f373f03e0e78280b7
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2024-12-06 22:21:04 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        int a =Integer.parseInt(st.nextToken());
+        int b =Integer.parseInt(st.nextToken());
+        int c =Integer.parseInt(st.nextToken());
+        int d =Integer.parseInt(st.nextToken());
+        int e =Integer.parseInt(st.nextToken());
+        
+        int number = (a*a+b*b+c*c+d*d+e*e)%10;
+        System.out.println(number);
+    }
+}
