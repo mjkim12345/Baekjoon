00a3176315d626e7e0ca28236c7315cbd6e0cdb4
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2025-03-20 15:08:34 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int home[15][15];
+
+int main(){
+ 
+    int c;
+    cin >> c;
+    for(int i=0; i<15; i++){
+        home[0][i] = i;
+    }
+
+    for(int i=1; i<15; i++){
+        for(int j=1; j<15; j++){
+            for(int k=1; k<=j; k++){
+                home[i][j] += home[i-1][k];
+            }
+        }
+    }
+
+    while(c--){
+        int a,b;
+        cin >> a >> b;
+        cout << home[a][b] << endl;
+    }
+
+
+    return 0;
+}
