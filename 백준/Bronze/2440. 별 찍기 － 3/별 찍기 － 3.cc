4c2585448587d1ee5db459f6140434ca00b9bf24
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2025-04-27 02:15:30 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -16,8 +16,8 @@ int main() {
     int N;
     cin >> N;
 
-    for (int i=N; N>0; N--) {
-        for (int j=0; j<N; j++) {
+    for (int i=N; i>0; i--) {
+        for (int j=0; j<i; j++) {
             cout << "*";
         }
         cout << "\n";
