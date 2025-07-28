111c03d6f718196c5bf9a12c647d122b7610af5a
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2025-07-29 03:22:46 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -4,42 +4,21 @@
 using namespace std;
 
 int N;
-string pt,in;
-vector<string> pt_r;
-string in_r[100];
-vector<string> result;
-
-void split(const string& input , string delimiter) {
-    auto start=0;
-    auto end = input.find(delimiter);
-    pt_r.push_back(input.substr(start, end-start));
-    start = end+delimiter.size();
-    pt_r.push_back(input.substr(start));
-}
-
+string s, ori_s , pre, suf;
 int main() {
-    cin >> N >> pt;
-    split(pt,"*");
+    cin >> N;
+    cin >> ori_s;
+    int pos = ori_s.find("*");
+    pre = ori_s.substr(0,pos);
+    suf = ori_s.substr(pos+1);
 
     for (int i=0; i<N; i++) {
-        cin >> in;
-
-        if (in.size()<pt_r[0].size()+pt_r[1].size()) {
-            cout << "NE" << "\n";
-            continue;
-        }
-
-        in_r[0] = in.substr(0,pt_r[0].size());
-        in_r[1] = in.substr(in.size()-pt_r[1].size());
-
-        if (in_r[0]==pt_r[0]) {
-            if (in_r[1]==pt_r[1]) {
-                cout << "DA" << "\n";
-            }else {
-                cout << "NE" << "\n";
-            }
+        cin >> s;
+        if (s.size()<pre.size()+suf.size()) {
+            cout << "NE\n";
         }else {
-            cout << "NE" << "\n";
+            if (pre == s.substr(0,pre.size())&& suf == s.substr(s.size()-suf.size())) cout << "DA\n";
+            else cout << "NE\n";
         }
     }
     return 0;
